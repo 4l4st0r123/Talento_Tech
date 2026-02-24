70d2fe546ff84d97e588e1e169aec93a1a9c466e
4l4stor123 <nahuelbilliani@gmail.com>
2026-02-24 00:15:48 -0300

    agregar imagenes

---
+++ JS/text.js
@@ -0,0 +1,13 @@
+function ChangeMouse() {
+    document.getElementById("button").style.cursor = "pointer";
+    //Button.style.width = "400px";
+}
+
+
+function myFunction() {
+    alert("¡Haz hecho clic en la imagen!");
+}
+
+const information = document.getElementById("info").outerHTML;
+
+console.log(information);
